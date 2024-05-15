88ae55160c684cc55131117fe1bdb904dd6aea51
Dorito <80398645+baksohyeon@users.noreply.github.com>
2024-05-15 16:54:22 +0900

    config: automating backup the solution file (#1)
    * fix: delete test coverage files which is redunant
    
    * git: create categorized directory by href
    
    - Created .gitkeep files to maintain empty directories.
    
    - Basically, just storing the code
    
    * script: Add dev:submit script, create fs.util.ts for file system operations
    
    This commit adds a new script "dev:submit" to run fs.util.ts for handling file system operations. It also introduces fs.util.ts with functions for creating files based on user input and a template file.
    
    * fix: Remove template name constant, add daytime timestamp, update user prompts and file creation logic. Add async to processAnswers function for reading files asynchronously.
    
    * fix: change default file name to day timestamp
    
    removed unused method
    
    * env: Update dev:submit script to submit script in package.json
    
    The dev:submit script was renamed to submit in the package.json file. This change simplifies the naming convention for running a specific TypeScript file.
    
    * style: reset code
    
    * script: delete redundant command at package.json
    
    * docs: add descriptions about script command

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # algorithm
+
+```bash
+yarn test # testing
+yarn dev # execute the solution function
+yarn submit # snapshot the app.ts with the notes (bash input required)
+```
---
+++ src/util/fs.util.ts
@@ -0,0 +1,118 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as readline from "readline";
+
+const DATE = new Date();
+
+const formatDigit = (time: number) => {
+  return time.toString().padStart(2, "0");
+};
+
+const TIMESTAMP = `${DATE.getFullYear()}-${formatDigit(
+  DATE.getMonth() + 1
+)}-${formatDigit(DATE.getDate())}`;
+
+const DAY_TIMESTAMP = `${formatDigit(DATE.getHours())}:${formatDigit(
+  DATE.getMinutes()
+)}`;
+
+interface ICommand {
+  question: string;
+  name: string;
+  defaultValue: string;
+}
+
+const PLATFORM_LIST = {
+  LeetCode: "LeetCode",
+  Programmers: "Programmers",
+} as const;
+
+const commands: ICommand[] = [
+  {
+    question:
+      "\n Press the letter of the platform (Programmers: p, LeetCode: l): ",
+    name: "platform",
+    defaultValue: PLATFORM_LIST.Programmers,
+  },
+  {
+    question:
+      "\n Configure solution directory name\n \t recommand solution name: ",
+    name: "destDir",
+    defaultValue: TIMESTAMP,
+  },
+  {
+    question: "\n Configure file name to store",
+    name: "destFile",
+    defaultValue: `timestamp:${DAY_TIMESTAMP}`,
+  },
+];
+
+const answers: { [key: string]: string } = {};
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const executeCommands = (index: number) => {
+  if (index === commands.length) {
+    rl.close();
+
+    processAnswers(answers);
+    return;
+  }
+
+  const { question, name, defaultValue } = commands[index];
+
+  rl.question(`${question} (default: ${defaultValue}): `, (answer) => {
+    // 플랫폼이 leetcode programmers 인지 확인
+    if (name === "platform") {
+      if (
+        answer.toLowerCase() === "p" ||
+        answer.toLowerCase() === "programmers"
+      ) {
+        answers[name] = PLATFORM_LIST.Programmers;
+      } else if (
+        answer.toLowerCase() === "l" ||
+        answer.toLowerCase() === "leetcode"
+      ) {
+        answers[name] = PLATFORM_LIST.LeetCode;
+      } else {
+        answers[name] = defaultValue;
+      }
+      // 그 외 다른 커맨드들은 그대로 저장
+    } else {
+      answers[name] = answer || defaultValue;
+    }
+    executeCommands(index + 1);
+  });
+};
+
+const processAnswers = async (answers: { [key: string]: string }) => {
+  const { platform, destDir, destFile } = answers;
+  // 플랫폼, 문제이름, 파일이름
+  const templatePath = path.resolve(
+    __dirname,
+    "../../src/util/REVIEW_TEMPLATE.md"
+  );
+
+  const solutionPath = path.resolve(__dirname, `../../src/app.ts`);
+
+  const targetPath = path.resolve(
+    __dirname,
+    `../../solution/${platform}/${destDir}`
+  );
+
+  const [templateContent, solutionContent] = await Promise.all([
+    fs.promises.readFile(templatePath, "utf8"),
+    fs.promises.readFile(solutionPath, "utf8"),
+  ]);
+
+  fs.mkdirSync(targetPath, { recursive: true });
+  fs.writeFileSync(`${targetPath}/${destFile}.solution.ts`, solutionContent);
+  fs.writeFileSync(`${targetPath}/${destFile}.note.md`, templateContent);
+
+  console.log("ok");
+};
+
+executeCommands(0);
