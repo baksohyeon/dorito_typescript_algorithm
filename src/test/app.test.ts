91472002ab250fe317ff0988f17fcf65e2b4c406
Dorito <80398645+baksohyeon@users.noreply.github.com>
2024-06-03 20:19:20 +0900

    [2024-06-03] 프로그래머스 - 옹알이(2) 풀이 (#3)
    * Update package.json with packageManager version and yarn details
    
    Added a new "packageManager" field in package.json specifying Yarn version 1.22.22 along with its SHA-512 checksum for verification purposes.
    
    * [프로그래머스] 옹알이(2) 풀이

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -1,30 +1,22 @@
 import { solution } from "../app";
 
 describe("solution", () => {
-  test("example 1", () => {
-    const bridge_length = 2;
-    const weight = 10;
-    const truck_weights = [7, 4, 5, 6];
-    const expected = 8;
-    const result = solution(bridge_length, weight, truck_weights);
+  it("test 1", () => {
+    const babbling = ["aya", "yee", "u", "maa"];
+    const result = solution(babbling);
+    const expected = 1;
     expect(result).toBe(expected);
   });
-
-  test("example 2", () => {
-    const bridge_length = 100;
-    const weight = 100;
-    const truck_weights = [10];
-    const expected = 101;
-    const result = solution(bridge_length, weight, truck_weights);
+  it("test 2", () => {
+    const babbling = ["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"];
+    const result = solution(babbling);
+    const expected = 2;
     expect(result).toBe(expected);
   });
-
-  test("example 3", () => {
-    const bridge_length = 100;
-    const weight = 100;
-    const truck_weights = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
-    const expected = 110;
-    const result = solution(bridge_length, weight, truck_weights);
+  it("test 2", () => {
+    const babbling = ["mama", "ayaye"];
+    const result = solution(babbling);
+    const expected = 1;
     expect(result).toBe(expected);
   });
 });
