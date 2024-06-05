0ba02dc497a28ba5bd94f5798ac1632143aaa11b
Dorito <80398645+baksohyeon@users.noreply.github.com>
2024-06-05 14:07:26 +0900

    [2024-06-04] 프로그래머스 - 오픈채팅방 문제 풀이 (#4)
    * solved: 프로그래머스 오픈채팅방
    
    https://school.programmers.co.kr/learn/courses/30/lessons/42888
    
    * note: backup the solution and documenting
    
    * Update code review notes to consider scalability.

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -2,21 +2,20 @@ import { solution } from "../app";
 
 describe("solution", () => {
   it("test 1", () => {
-    const babbling = ["aya", "yee", "u", "maa"];
-    const result = solution(babbling);
-    const expected = 1;
-    expect(result).toBe(expected);
-  });
-  it("test 2", () => {
-    const babbling = ["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"];
-    const result = solution(babbling);
-    const expected = 2;
-    expect(result).toBe(expected);
-  });
-  it("test 2", () => {
-    const babbling = ["mama", "ayaye"];
-    const result = solution(babbling);
-    const expected = 1;
-    expect(result).toBe(expected);
+    const record = [
+      "Enter uid1234 Muzi",
+      "Enter uid4567 Prodo",
+      "Leave uid1234",
+      "Enter uid1234 Prodo",
+      "Change uid4567 Ryan",
+    ];
+    const result = solution(record);
+    const expected = [
+      "Prodo님이 들어왔습니다.",
+      "Ryan님이 들어왔습니다.",
+      "Prodo님이 나갔습니다.",
+      "Prodo님이 들어왔습니다.",
+    ];
+    expect(result).toEqual(expected);
   });
 });
