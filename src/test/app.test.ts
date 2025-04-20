a9e0862017402264ab257fff0d0ec1b0783b502b
Dorito <80398645+baksohyeon@users.noreply.github.com>
2025-04-20 20:10:44 +0900

    [프로그래머스] Lv3: 코딩테스트 연습 > 해시 > 베스트앨범 (#6)
    - link: https://school.programmers.co.kr/learn/courses/30/lessons/42579

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -2,25 +2,12 @@ import solution from "../app";
 
 describe("solution", () => {
   it("test 1", () => {
-    const result = solution(1, 2, 3, 4);
-    const expected = [5, 4];
+    const genres = ["classic", "pop", "classic", "classic", "pop"]
+    const plays = [500, 600, 150, 800, 2500]
+    const expected = [4, 1, 3, 0];
+
+    const result = solution(genres, plays);
     expect(result).toEqual(expected);
   });
 
-  it("test 2", () => {
-    const result = solution(2, 1, 2, 1);
-    const expected = [4, 1];
-    expect(result).toEqual(expected);
-  });
-
-  it("test 3", () => {
-    const result = solution(4, 4, 4, 4);
-    const expected = [2, 1];
-    expect(result).toEqual(expected);
-  });
-  it("test 4", () => {
-    const result = solution(1, 6, 2, 6);
-    const expected = [1, 2];
-    expect(result).toEqual(expected);
-  });
 });
