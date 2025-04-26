2cee33bbc3e99fc2620a032ec7ed5bf5b78a2cb9
Dorito <bori12370@gmail.com>
2025-04-26 12:20:08 +0900

    [프로그래머스] 코딩테스트 연습 깊이/너비 우선 탐색(DFS/BFS) 게임 맵 최단거리
    - code

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -2,13 +2,26 @@ import { solution } from "../app";
 
 describe("solution", () => {
 
-  it('should return 5 when numbers are [1, 1, 1, 1, 1] and target is 3', () => {
-    expect(solution([1, 1, 1, 1, 1], 3)).toBe(5);
+  it(`should return 11 when the 'map' argument is provided as specified`, () => {
+    const map = [
+      [1, 0, 1, 1, 1],
+      [1, 0, 1, 0, 1],
+      [1, 0, 1, 1, 1],
+      [1, 1, 1, 0, 1],
+      [0, 0, 0, 0, 1]
+    ];
+    expect(solution(map)).toBe(11);
   });
 
-  it('should return 2 when numbers are [4, 1, 2, 1] and target is 4', () => {
-    expect(solution([4, 1, 2, 1], 4)).toBe(2);
+  it(`should return -1 when the 'map' argument is provided as specified`, () => {
+    const map = [
+      [1, 0, 1, 1, 1],
+      [1, 0, 1, 0, 1],
+      [1, 0, 1, 1, 1],
+      [1, 1, 1, 0, 0],
+      [0, 0, 0, 0, 1]
+    ];
+    expect(solution(map)).toBe(-1);
   });
-
 
 });
