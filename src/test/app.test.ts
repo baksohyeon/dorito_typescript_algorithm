7a1248ade855b9e1211a171ca1dc5e8753033293
Dorito <bori12370@gmail.com>
2025-07-27 13:18:52 +0900

    [Lv.1] Programmers: 택배 상자 꺼내기

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -1,19 +1,21 @@
 import { solution } from "../app";
 
 describe("should return the number of ways to reach the target sum with the given numbers and target sum", () => {
-  it(`should return 5 for [1, 1, 1, 1, 1] and 3`, () => {
-    expect(solution([1, 1, 1, 1, 1], 3)).toBe(5);
+  it.only(`should return 3`, () => {
+    const n = 22;
+    const w = 6;
+    const num = 8;
+    const result = solution(n, w, num);
+    const expected = 3;
+    expect(result).toEqual(expected);
   });
 
-  it(`should return 2 for [4, 1, 2, 1] and 4`, () => {
-    expect(solution([4, 1, 2, 1], 4)).toBe(2);
-  });
-
-  it(`should return 0 for [1, 2, 3] and 7`, () => {
-    expect(solution([1, 2, 3], 7)).toBe(0);
-  });
-
-  it(`should return 1 for [1, 1, 1, 1, 1] and 5`, () => {
-    expect(solution([1, 1, 1, 1, 1], 5)).toBe(1);
+  it(`should return 4`, () => {
+    const n = 13;
+    const w = 3;
+    const num = 6;
+    const result = solution(n, w, num);
+    const expected = 4;
+    expect(result).toEqual(expected);
   });
 });
